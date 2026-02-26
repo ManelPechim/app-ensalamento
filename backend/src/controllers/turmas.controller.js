7c6befc3caadf6330c3f4096dd5880c13b5ae660
ManelPechim <emanuel.pechim@grupointegrado.br>
2026-02-26 17:22:09 -0300

    chore: algumas mudanças e arrumando o metodo post da funcao createTurmas

--- backend/src/controllers/turmas.controller.js
+++ backend/src/controllers/turmas.controller.js
@@ -2,14 +2,14 @@ import supabase from '../config/supabase.js'
 
 export const getTurmas = async (req, res) => {
     const { data, error } = await supabase
-        .from('turmas')
-        .select('*');
+    .from('turmas')
+    .select('*');
 
-        if(error){
-            return res.status(500).json({ error: error.message });
-        }; 
+    if(error){
+        return res.status(500).json({ error: error.message });
+    }; 
         
-        return res.status(200).json({ turmas: data }); 
+    return res.status(200).json({ turmas: data }); 
 };
 
 export const createTurmas = async (req, res) => {
@@ -17,11 +17,11 @@ export const createTurmas = async (req, res) => {
 
     const { data, error } = await supabase
     .from('turmas')
-    .select({ nome, qtd_alunos, curso });
-
-    if(error){
-            return res.status(500).json({ error: error.message });
-        }; 
+    .insert([{ nome, qtd_alunos, curso }]);
         
-        return res.status(201).json({ message: 'Turma inserida com sucesso', data}); 
+    if(error){
+        return res.status(500).json({ error: error.message });
+    }; 
+        
+    return res.status(201).json({ message: 'Turma inserida com sucesso', data}); 
 }
