a15154ca1ccb02151786b49e1814ebb8bd8a4004
ManelPechim <ferreiraemanuel798@gmail.com>
2026-05-21 01:05:04 -0300

    style: mensagem no log do server, agora implícito que é um servidor local XD

--- backend/server.ts
+++ backend/server.ts
@@ -7,7 +7,7 @@ const PORT: number = parseInt(`${process.env.PORT}`) || 3000;
 server.listen(PORT, () => {
   console.log(`
     ==================================================
-    Servidor rodando na porta http://localhost:${PORT}
+    Servidor local rodando na porta http://localhost:${PORT}
     ==================================================
     `
   );
